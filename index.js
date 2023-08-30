f63ed0369159d57e4e2f5956be277f7c4a34c717
mdjahiulislam <jahidj2255@gmail.com>
2023-08-30 19:36:16 +0600

    data send to server and receiving data from server by node

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
+const cors = require('cors')
 const app = express();
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-    res.send('Welcome To Node journey')
+const users = ['jahid', 'james', 'jony', 'jahidul islam', 'kusaer']
+
+app.use(cors())
+app.use(bodyParser.json())
+
+// get method
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    const userName = users[userId];
+     res.send({userId, userName})
 })
 
-app.listen(4500, () => console.log('running on port 3000'))
+// Post method
+app.post('/postUser/', (req, res) =>{
+    const user = req.body;
+    user.id = '1';
+    res.send(user)
+    console.log('get data from client',req.body)
+})
+
+app.listen(3000, () => console.log('running on port 3000'))
